d5aad078c97db9d08626b748d76de52c1cad4855
honghainguyen777 <n.h.hai2015@gmail.com>
2021-05-13 10:24:40 +0200

    completed the decision tree with returned ticket

--- src/components/Questionnaire/DecisionTree.js
+++ src/components/Questionnaire/DecisionTree.js
@@ -1,6 +1,5 @@
 function findNextQuestion(currentQuestionId, answerPool) {
   let answer = answerPool[currentQuestionId];
-  console.log(answerPool);
 
   switch (currentQuestionId) {
     case 0:
@@ -45,51 +44,63 @@ function findNextQuestion(currentQuestionId, answerPool) {
 
       // for long trips
       return 18;
-    case 14:
-      return;
-    case 15:
-      return;
+    // case 14:
+    //   return;
+    // case 15:
+    //   return;
     case 18:
       // if adult is true and duration of stay is in hours
       if (answerPool[13] && answerPool[1] === "hours") {
         // if not multiple destinations or no return -
         if (!answerPool[7]) {
-          if (answer === "AB") return; // ticket T1_AB
-          if (answer === "BC") return; // ticket T1_BC
-          return; // ticket T1_ABC
+          if (answer === "AB")
+            return { ticketId: 1, zone: "AB", fareType: "normal" }; // ticket T1_AB
+          if (answer === "BC")
+            return { ticketId: 1, zone: "BC", fareType: "normal" }; // ticket T1_BC
+          return { ticketId: 1, zone: "ABC", fareType: "normal" }; // ticket T1_ABC
         }
         // multiple destinations and return - number of trip greater than 2
         if (answerPool[7] && answerPool[10] > 2) {
-          if (answer === "AB") return; // ticket T5_AB
-          if (answer === "BC") return; // ticket T5_BC
-          return; // ticket T5_ABC
+          if (answer === "AB")
+            return { ticketId: 5, zone: "AB", fareType: "normal" }; // ticket T5_AB
+          if (answer === "BC")
+            return { ticketId: 5, zone: "BC", fareType: "normal" }; // ticket T5_BC
+          return { ticketId: 5, zone: "ABC", fareType: "normal" }; // ticket T5_ABC
         }
         // multiple destinations and return - number of trip greater equal 1 or 2
         if (answerPool[7] && answerPool[10] <= 2) {
-          if (answer === "AB") return; // 2 x ticket T1_AB
-          if (answer === "BC") return; // 2 x ticket T1_BC
-          return; // 2 x ticket T1_ABC
+          if (answer === "AB")
+            return { ticketId: 1, zone: "AB", fareType: "normal" }; // 2 x ticket T1_AB
+          if (answer === "BC")
+            return { ticketId: 1, zone: "BC", fareType: "normal" }; // 2 x ticket T1_BC
+          return { ticketId: 1, zone: "ABC", fareType: "normal" }; // 2 x ticket T1_ABC
         }
       }
       // if adult is false and duration of stay is in hours -> reduced tickets
       if (!answerPool[13] && answerPool[1] === "hours") {
         // if not multiple destinations or no return -
         if (!answerPool[7]) {
-          if (answer === "AB") return; // ticket T1_AB1
-          if (answer === "BC") return; // ticket T1_BC1
-          return; // ticket T1_ABC1
+          if (answer === "AB")
+            return { ticketId: 1, zone: "AB", fareType: "reduced" }; // ticket T1_AB1
+          if (answer === "BC")
+            return { ticketId: 1, zone: "BC", fareType: "reduced" }; // ticket T1_BC1
+          return { ticketId: 1, zone: "ABC", fareType: "reduced" }; // ticket T1_ABC1
         }
         // multiple destinations and return - number of trip greater than 2
         if (answerPool[7] && answerPool[10] > 2) {
-          if (answer === "AB") return; // ticket T5_AB1
-          if (answer === "BC") return; // ticket T5_BC1
-          return; // ticket T5_ABC
+          if (answer === "AB")
+            return { ticketId: 5, zone: "AB", fareType: "reduced" }; // ticket T5_AB1
+          if (answer === "BC")
+            return { ticketId: 5, zone: "BC", fareType: "reduced" }; // ticket T5_BC1
+          return { ticketId: 5, zone: "ABC", fareType: "reduced" }; // ticket T5_ABC
         }
         // multiple destinations and return - number of trip greater equal 1 or 2
         if (answerPool[7] && answerPool[10] <= 2) {
-          if (answer === "AB") return; // 2 x ticket T1_AB1
-          if (answer === "BC") return; // 2 x ticket T1_BC1
-          return; // 2 x ticket T1_ABC
+          if (answer === "AB")
+            return { ticketId: 1, zone: "AB", fareType: "reduced" }; // 2 x ticket T1_AB1
+          if (answer === "BC")
+            return { ticketId: 1, zone: "BC", fareType: "reduced" }; // 2 x ticket T1_BC1
+          return { ticketId: 1, zone: "ABC", fareType: "reduced" }; // 2 x ticket T1_ABC
         }
       }
 
@@ -99,40 +110,52 @@ function findNextQuestion(currentQuestionId, answerPool) {
         if (answerPool[3] < 5) {
           // if travel often or number of trips per day greater than 2
           if (answerPool[6] || answerPool[11] > 2) {
-            if (answer === "AB") return; // ticket T5_AB
-            if (answer === "BC") return; // ticket T5_BC
-            return; // ticket T5_ABC
+            if (answer === "AB")
+              return { ticketId: 5, zone: "AB", fareType: "normal" }; // ticket T5_AB
+            if (answer === "BC")
+              return { ticketId: 5, zone: "BC", fareType: "normal" }; // ticket T5_BC
+            return { ticketId: 5, zone: "ABC", fareType: "normal" }; // ticket T5_ABC
           }
           if (!answerPool[6] && answerPool[11] <= 2) {
-            if (answer === "AB") return; // ticket T1_AB
-            if (answer === "BC") return; // ticket T1_BC
-            return; // ticket T1_ABC
+            if (answer === "AB")
+              return { ticketId: 1, zone: "AB", fareType: "normal" }; // ticket T1_AB
+            if (answer === "BC")
+              return { ticketId: 1, zone: "BC", fareType: "normal" }; // ticket T1_BC
+            return { ticketId: 1, zone: "ABC", fareType: "normal" }; // ticket T1_ABC
           }
         }
         // multiple destinations and return - number of trips between 5 and 14 days
         if (answerPool[3] >= 5 && answerPool[3] <= 14) {
           // if travel often or number of trips per day greater than 2
           if (answerPool[6] || answerPool[11] > 2) {
-            if (answer === "AB") return; // ticket T7_AB
-            if (answer === "BC") return; // ticket T7_BC
-            return; // ticket T7_ABC
+            if (answer === "AB")
+              return { ticketId: 7, zone: "AB", fareType: "normal" }; // ticket T7_AB
+            if (answer === "BC")
+              return { ticketId: 7, zone: "BC", fareType: "normal" }; // ticket T7_BC
+            return { ticketId: 7, zone: "ABC", fareType: "normal" }; // ticket T7_ABC
           }
           if (!answerPool[6] && answerPool[11] <= 2) {
-            if (answer === "AB") return; // ticket T1_AB
-            if (answer === "BC") return; // ticket T1_BC
-            return; // ticket T1_ABC
+            if (answer === "AB")
+              return { ticketId: 1, zone: "AB", fareType: "normal" }; // ticket T1_AB
+            if (answer === "BC")
+              return { ticketId: 1, zone: "BC", fareType: "normal" }; // ticket T1_BC
+            return { ticketId: 1, zone: "ABC", fareType: "normal" }; // ticket T1_ABC
           }
         }
 
         if (answerPool[3] > 14) {
           if (answerPool[12]) {
-            if (answer === "AB") return; // ticket T9_AB
-            if (answer === "BC") return; // ticket T9_BC
-            return; // ticket T9_ABC
+            if (answer === "AB")
+              return { ticketId: 9, zone: "AB", fareType: "normal" }; // ticket T9_AB
+            if (answer === "BC")
+              return { ticketId: 9, zone: "BC", fareType: "normal" }; // ticket T9_BC
+            return { ticketId: 9, zone: "ABC", fareType: "normal" }; // ticket T9_ABC
           } else {
-            if (answer === "AB") return; // ticket T8_AB
-            if (answer === "BC") return; // ticket T8_BC
-            return; // ticket T8_ABC
+            if (answer === "AB")
+              return { ticketId: 8, zone: "AB", fareType: "normal" }; // ticket T8_AB
+            if (answer === "BC")
+              return { ticketId: 8, zone: "BC", fareType: "normal" }; // ticket T8_BC
+            return { ticketId: 8, zone: "ABC", fareType: "normal" }; // ticket T8_ABC
           }
         }
       }
@@ -143,40 +166,52 @@ function findNextQuestion(currentQuestionId, answerPool) {
         if (answerPool[3] < 5) {
           // if travel often or number of trips per day greater than 2
           if (answerPool[6] || answerPool[11] > 2) {
-            if (answer === "AB") return; // ticket T5_AB1
-            if (answer === "BC") return; // ticket T5_BC1
-            return; // ticket T5_ABC1
+            if (answer === "AB")
+              return { ticketId: 5, zone: "AB", fareType: "reduced" }; // ticket T5_AB1
+            if (answer === "BC")
+              return { ticketId: 5, zone: "BC", fareType: "reduced" }; // ticket T5_BC1
+            return { ticketId: 5, zone: "ABC", fareType: "reduced" }; // ticket T5_ABC1
           }
           if (!answerPool[6] && answerPool[11] <= 2) {
-            if (answer === "AB") return; // ticket T1_AB1
-            if (answer === "BC") return; // ticket T1_BC1
-            return; // ticket T1_ABC1
+            if (answer === "AB")
+              return { ticketId: 1, zone: "AB", fareType: "reduced" }; // ticket T1_AB1
+            if (answer === "BC")
+              return { ticketId: 1, zone: "BC", fareType: "reduced" }; // ticket T1_BC1
+            return { ticketId: 1, zone: "ABC", fareType: "reduced" }; // ticket T1_ABC1
           }
         }
         // multiple destinations and return - number of trips between 5 and 14 days
         if (answerPool[3] >= 5 && answerPool[3] <= 14) {
           // if travel often or number of trips per day greater than 2
           if (answerPool[6] || answerPool[11] > 2) {
-            if (answer === "AB") return; // ticket T7_AB1
-            if (answer === "BC") return; // ticket T7_BC1
-            return; // ticket T7_ABC
+            if (answer === "AB")
+              return { ticketId: 7, zone: "AB", fareType: "reduced" }; // ticket T7_AB1
+            if (answer === "BC")
+              return { ticketId: 7, zone: "BC", fareType: "reduced" }; // ticket T7_BC1
+            return { ticketId: 7, zone: "ABC", fareType: "reduced" }; // ticket T7_ABC1
           }
           if (!answerPool[6] && answerPool[11] <= 2) {
-            if (answer === "AB") return; // ticket T1_AB1
-            if (answer === "BC") return; // ticket T1_BC1
-            return; // ticket T1_ABC1
+            if (answer === "AB")
+              return { ticketId: 1, zone: "AB", fareType: "reduced" }; // ticket T1_AB1
+            if (answer === "BC")
+              return { ticketId: 1, zone: "BC", fareType: "reduced" }; // ticket T1_BC1
+            return { ticketId: 1, zone: "ABC", fareType: "reduced" }; // ticket T1_ABC1
           }
         }
 
         if (answerPool[3] > 14) {
           if (answerPool[12]) {
-            if (answer === "AB") return; // ticket T9_AB1
-            if (answer === "BC") return; // ticket T9_BC1
-            return; // ticket T9_ABC1
+            if (answer === "AB")
+              return { ticketId: 9, zone: "AB", fareType: "reduced" }; // ticket T9_AB1
+            if (answer === "BC")
+              return { ticketId: 9, zone: "BC", fareType: "reduced" }; // ticket T9_BC1
+            return { ticketId: 9, zone: "ABC", fareType: "reduced" }; // ticket T9_ABC1
           } else {
-            if (answer === "AB") return; // ticket T8_AB1
-            if (answer === "BC") return; // ticket T8_BC1
-            return; // ticket T8_ABC1
+            if (answer === "AB")
+              return { ticketId: 8, zone: "AB", fareType: "reduced" }; // ticket T8_AB1
+            if (answer === "BC")
+              return { ticketId: 8, zone: "BC", fareType: "reduced" }; // ticket T8_BC1
+            return { ticketId: 8, zone: "ABC", fareType: "reduced" }; // ticket T8_ABC1
           }
         }
       }
@@ -184,41 +219,24 @@ function findNextQuestion(currentQuestionId, answerPool) {
       // if duration of stay is in months
       if (answerPool[1] === "months") {
         if (answerPool[12]) {
-          if (answer === "AB") return; // ticket T9_AB1
-          if (answer === "BC") return; // ticket T9_BC1
-          return; // ticket T9_ABC1
+          if (answer === "AB")
+            return { ticketId: 9, zone: "AB", fareType: "reduced" }; // ticket T9_AB1
+          if (answer === "BC")
+            return { ticketId: 9, zone: "BC", fareType: "reduced" }; // ticket T9_BC1
+          return { ticketId: 9, zone: "ABC", fareType: "reduced" }; // ticket T9_ABC1
         } else {
-          if (answer === "AB") return; // ticket T8_AB1
-          if (answer === "BC") return; // ticket T8_BC1
-          return; // ticket T8_ABC1
+          if (answer === "AB")
+            return { ticketId: 8, zone: "AB", fareType: "reduced" }; // ticket T8_AB1
+          if (answer === "BC")
+            return { ticketId: 8, zone: "BC", fareType: "reduced" }; // ticket T8_BC1
+          return { ticketId: 8, zone: "ABC", fareType: "reduced" }; // ticket T8_ABC1
         }
       }
 
-      return 0;
+      return;
     default:
-      return 0;
+      return;
   }
 }
 
 export { findNextQuestion };
-
-// example of storing questions and answers
-// const store_question_answer = [
-//     {"question": 1, "answer": "hours"}, // hours,
-//     {"question": 2, "answer": ""},
-//     {"question": 3, "answer": 3}, // 1-4 days, 5-7 days, 7-14 days, > 14-days
-//     {"question": 4, "answer": 2},
-//     {"question": 5, "answer": "S-Bahn"}, //S-Bahn, trams, combined
-//     {"question": 6, "answer": "Yes"}, // Yes or No, True or False
-//     {"question": 7, "answer": "Yes"}, // Yes or No, True or False
-//     {"question": 8, "answer": "Yes"}, // Yes or No, True or False
-//     {"question": 9, "answer": "Yes"}, // Yes or No, True or False
-//     {"question": 10, "answer": 2}, // 2, 3-4, > 4
-//     {"question": 11, "answer": 2}, // 1 - 2, >= 3
-//     {"question": 12, "answer": "Yes"}, // Yes or No, True or False
-//     {"question": 13, "answer": "Yes"}, // Yes or No, True or False
-//     {"question": 14, "answer": "Yes"}, // Yes or No, True or False
-//     {"question": 15, "answer": "Yes"}, // Yes or No, True or False
-//     {"question": 16, "answer": "Yes"}, // Yes or No, True or False
-//     {"question": 17, "answer": 3},
-// ]
